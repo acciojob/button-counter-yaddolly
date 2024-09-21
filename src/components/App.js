f953d580dd27f6c7caebb0aac2452c4ea4050018
AAKASH _YADAV <yaddolly>
2024-09-21 20:07:47 +0530

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from 'react'
 
 const App = () => {
+  const[count, setCount] = useState(0)
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>Button clicked {count} times</p>
+      <button onClick={() => {
+        setCount(count + 1)
+      }}>Click me</button>
     </div>
   )
 }
